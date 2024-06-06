fad026e8211ddc9faa9af4b2729f4d8a4c8d0192
vmarcos7 <167425877+vmarcos7@users.noreply.github.com>
2024-06-06 08:00:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorAll(".botao");
+const conteudo = documento.querySelectorAll(".aba-conteudo")
+for (let i = 0; i < botoes.lenght ; i++)
+    botoes [i].onclick = function(){}
+for (let j = 0 ; j < botoes.lenght ; j++){}
+botoes[j].classList.remove("ativo");
+conteudo[j].classList.remove("ativo")
+
+botoes[i]
+
