4163fe3f732acb82138eaaf0b356b06c43c9db72
vmarcos7 <167425877+vmarcos7@users.noreply.github.com>
2024-07-25 08:05:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
-const tempoObjetiv1 = new Date(2024,11,31,59,59);
+const tempoObjetivo1 = new Date(2023,11,31,59,59);
 const tempoObjetivo2 = new Date(2024,10,28,59,59);
 const tempoObjetivo3 = new Date(2024,9,29,59,59);
 const tempoObjetivo4 = new Date(2024,8,27,59,59);
@@ -12,25 +12,28 @@ let minutos;
 let horas;
 let dias;
 segundos = (tempoObjetivo-agora)/1000
-minutos = segundos/60
-horas = minutos/60
-dias = horas/24
+minutos = (segundos/60)
+horas = (minutos/60)
+dias = (horas/24)
 segundos = Math.floor(segundos)
 minutos = Math.floor(minutos)
 horas = Math.floor(horas)
 dias = Math.floor(dias)
+
 segundos = segundos%60
 minutos = minutos%60
 horas = horas%24
 
-
+if (segundos >=0) {
 return `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
-
+}else{ 
+    return 'Prazo Finalizado!'
 }
-contador[0].textContent = calculaTempo(tempoObjetivo1);
-contador[1].textContent = calculaTempo(tempoObjetivo2);
-contador[2].textContent = calculaTempo(tempoObjetivo3);
-contador[3].textContent = calculaTempo(tempoObjetivo4);
+}
+tempo[0].textContent = calculaTempo(tempoObjetivo1);
+tempo[1].textContent = calculaTempo(tempoObjetivo2);
+tempo[2].textContent = calculaTempo(tempoObjetivo3);
+tempo[3].textContent = calculaTempo(tempoObjetivo4);
 
 
 for(let i = 0; i < botoes.length ; i++){
